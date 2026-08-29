f175ef67f2c7b5434844cd13db41ab52b05a4cc7
Xeno-bytes <santos.223361@sjdelmonte.sti.edu.ph>
2026-08-29 22:53:50 +0800

    small changes

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCsvagcE4Quq822oDy0OYSZNmGzd1K9DBk",
-  authDomain: "checkpoint-app-cd6fb.firebaseapp.com",
-  projectId: "checkpoint-app-cd6fb",
-  storageBucket: "checkpoint-app-cd6fb.firebasestorage.app",
-  messagingSenderId: "52673405783",
-  appId: "1:52673405783:web:302aefc8eee95b69db9412",
-  measurementId: "G-MBE20J4S5Q"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
